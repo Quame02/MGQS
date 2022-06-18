9c9b9b9387b40a6cb501cdbae9fba191999ee6c4
Quame02 <eliasbashirudeen66@gmail.com>
2022-06-18 10:28:22 +0100

    first commit

---
+++ Question!/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    string firstWord = null ;
+         //   string secondWord = "";
+
+         //   for (int i = 0; i < 2; i++)
+         //   {
+                
+            //    if (i==0)
+            // {
+            //         Console.WriteLine("type in  first  word  press Enter to submit ");
+            //       firstWord = Console.ReadLine();
+            // }
+            //  else
+            // {
+            //  Console.WriteLine("type in  second word  press Enter to submit ");
+            //  secondWord = Console.ReadLine();
+            // }
+            //  }
+            // int firstWordLength = firstWord.Length;
+            // int secondWordLength = secondWord.Length;
+
+            // int equivalentLength = firstWordLength + secondWordLength;
+            
+            // if (equivalentLength > 20 )
+            // {
+            //  Console.WriteLine(equivalentLength *3 );
+            // }
+            //    else
+            // {
+            //   Console.WriteLine(equivalentLength + 20 );
+            
+            // // using tenary operator
+           //  Console.WriteLine(equivalentLength > 20 ? equivalentLength *3 :equivalentLength + 20);
+
+           // Write a program to calculate the profit between the cost price of a product of $200.876 whose selling price is $255.425. The result should be in two decimal places.
+            
+             //float CPf = 200.876f;
+             //float SPf = 255.425f;
+           
+             //float equivalent2f = (SPf - CPf);
+             //Console.WriteLine($"{SPf} - {CPf} = ${equivalent2f}");
+
+             //string CP = "$200.876";
+             //string SP = "$255.425";
+             //decimalVar profit = decimalVar.ToString($"{CP} - {SP}");
+             //Console.WriteLine(profit);
+
+             //  Write a program that reads two numbers from the console and prints the 
+             //   greater of them. Solve the problem without using conditional or the tenary operator
+             //    statements.
+             
+              // Console.Write("Enter in first number to compare");
+              // double firstNumber = double.Parse(Console.ReadLine());
+              // Console.Write("Enter in second number to compare");
+              // double secondNumber = double.Parse(Console.ReadLine());
+
+              // double compareNumbers = Math.Max(firstNumber, secondNumber);
+              // Console.WriteLine("Number {0} is greater", compareNumbers);
+
+            
+
+        }
+    }
+}
